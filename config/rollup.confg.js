4ea599e8a6ae76d8862b7351769ba27018207c83
jdeseva <zjmqydes@163.com>
2019-11-28 11:02:30 +0800

    打包配置

--- config/rollup.confg.js
+++ config/rollup.confg.js
@@ -1,4 +1,5 @@
-// const path = require('path')
+import { uglify } from 'rollup-plugin-uglify'
+const path = require('path')
 const less = require('rollup-plugin-less')
 const babel = require('rollup-plugin-babel')
 const nodeResolve = require('rollup-plugin-node-resolve')
@@ -7,14 +8,21 @@ const replace = require('rollup-plugin-replace')
 
 const env = process.env.NODE_ENV
 
+const resolveFile = function (filePath) {
+  return path.join(__dirname, '..', filePath)
+}
+
 module.exports = [
   {
-    input: '../src/js/editor/index.js',
-    output: {
-      format: 'umd', //  五种输出格式：amd /  es6 / iife / umd / cjs
-      name: '$Editor', // 当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.A=...
-      sourcemap: true // 生成bundle.map.js文件，方便调试
-    },
+    input: resolveFile('/src/js/index.js'),
+    output: [
+      {
+        file: '../dist/build.umd.js',
+        format: 'umd', //  五种输出格式：amd /  es6 / iife / umd / cjs
+        name: '$Editor' // 当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.A=...
+        // sourcemap: true // 生成bundle.map.js文件，方便调试
+      }
+    ],
     external: [], // 外部依赖
     globals: {
       // react: 'React',
@@ -22,15 +30,15 @@ module.exports = [
     },
     plugins: [
       less(),
-      babel({ exclude: '**/node_modules/**' }),
-      nodeResolve({
-        jsnext: true,
-        main: true
+      babel({
+        exclude: 'node_modules/**'
       }),
+      nodeResolve(),
       commonjs({ include: /node_modules/ }),
       replace({
         'process.env.NODE_ENV': JSON.stringify(env)
-      })
+      }),
+      uglify()
     ]
   }
 ]
